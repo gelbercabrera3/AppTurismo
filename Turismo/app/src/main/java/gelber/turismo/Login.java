505bfff3ea28f586a69bca561463f5b211932cf1
Gelber <gelber411@hotmail.com>
2016-06-19 22:53:04 -0600

    Login
    Login funcional

--- Turismo/app/src/main/java/gelber/turismo/Login.java
+++ Turismo/app/src/main/java/gelber/turismo/Login.java
@@ -1,13 +1,99 @@
 package gelber.turismo;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import gelber.turismo.bean.*;
+import gelber.turismo.volley.WebService;
 
 public class Login extends AppCompatActivity {
 
+    private TextView txtCorreo, txtPassword;
+    private Button btnLogin, btnRegistro;
+    private Usuario userLogged = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstance){
+        getSupportActionBar().hide();
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistro = (Button) findViewById(R.id.btnRegistro);
+        txtCorreo = (TextView) findViewById(R.id.txtCorreo);
+        txtPassword = (TextView) findViewById(R.id.txtPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View v){
+                Map<String, String> params = new HashMap<String, String>();
+                String correo = txtCorreo.getText().toString();
+                String password = txtPassword.getText().toString();
+                params.put("nick", txtCorreo.getText().toString());
+                params.put("contrasena", txtPassword.getText().toString());
+
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, WebService.autenticar,
+                        new JSONObject(params), new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            JSONArray listaUsuarios=response.getJSONArray("user");
+                            if(listaUsuarios.length()>0){
+                                JSONObject user=listaUsuarios.getJSONObject(0);
+                                //Snackbar.make(, "Bienvenido " + user.getString("nick"), Snackbar.LENGTH_LONG).show();
+                                userLogged =new Usuario(
+                                      user.getInt("id_Usuario"),
+                                        user.getString("nombre"),
+                                        user.getString("nick"),
+                                        user.getString("correo"),
+                                        user.getString("contrasena"),
+                                        response.getString("token"),
+                                        response.getString("exp")
+                                );
+                                startActivity(new Intent(Login.this, Inicio.class));
+                            }else{
+                                Snackbar.make(v, "Verifique sus credenciales", Snackbar.LENGTH_LONG).show();
+                            }
+                        }catch(Exception ex){
+                            Log.e("Response exception ",ex.getMessage());
+                        }
+
+                    }
+                }, new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError err){
+                       // Log.d("Error:", err.getMessage());
+                    }
+                }
+
+                );
+                WebService.getInstance(v.getContext()).addToRequestQueue(request);
+            }
+        });
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Inicio.class));
+            }
+        });
     }
+
 }
---
+++ Turismo/app/src/main/java/gelber/turismo/volley/WebService.java
@@ -0,0 +1,62 @@
+package gelber.turismo.volley;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class WebService {
+    public static String server="http://192.168.1.8:3000/api/v1/usuario/";
+    public static String autenticar=server+"login";
+    private static WebService mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
+
+    private WebService(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized WebService getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new WebService(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
